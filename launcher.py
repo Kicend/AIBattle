971a12503c7a6a761f245dd7fa548e851b705130
Kicend <kicend@gmail.com>
2020-11-25 17:35:06 +0100

    fix bug with create config

--- launcher.py
+++ launcher.py
@@ -1,13 +1,16 @@
 import json
-from os import path
+import os
 from game import main
+
+version = "0.1-1"
 
 default_config = {"ai_1_script_path": "game/AI/A.W.L.B.py",
                   "ai_2_script_path": "game/AI/A.W.L.B.py"}
 
 
 def startup():
-    if not path.isfile("game/settings/config.json"):
+    os.makedirs("game/settings", exist_ok=True)
+    if not os.path.isfile("game/settings/config.json"):
         with open("game/settings/config.json", "a") as f:
             json.dump(default_config, f, indent=4)
 
