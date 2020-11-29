468746837de2f1479fea4a600c3de143cb474b1b
Kicend <kicend@gmail.com>
2020-11-29 21:05:10 +0100

    Read config

--- launcher.py
+++ launcher.py
@@ -2,7 +2,7 @@ import json
 import os
 from game import main
 
-version = "0.1-1"
+version = "0.2"
 
 default_config = {"ai_1_script_path": "game/AI/A.W.L.B.py",
                   "ai_2_script_path": "game/AI/A.W.L.B.py"}
@@ -14,11 +14,16 @@ def startup():
         with open("game/settings/config.json", "a") as f:
             json.dump(default_config, f, indent=4)
 
+    with open("game/settings/config.json", "r") as f:
+        game_config = json.load(f)
+
+    return game_config
+
 
 def update():
     pass
 
 
 if __name__ == "__main__":
-    startup()
-    main.main()
+    config = startup()
+    main.main(config)
