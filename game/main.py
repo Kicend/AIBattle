f8208439e86e8bf73afe5469d00d4a0df0a76ffa
Kicend <kicend@gmail.com>
2020-11-29 21:05:23 +0100

    Get scripts paths

--- game/main.py
+++ game/main.py
@@ -4,9 +4,9 @@ import time
 from game import game
 
 
-def main():
-    player1 = "game/AI/A.W.L.B.py"
-    player2 = "game/AI/A.W.L.B.py"
+def main(game_config):
+    player1 = game_config["ai_1_script_path"]
+    player2 = game_config["ai_2_script_path"]
     session = game.Game(False, "game/maps/map1.json", player1, player2)
     session.preparing()
     time.sleep(30)
